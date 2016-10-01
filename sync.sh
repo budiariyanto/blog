81c598475fddbc63404e2d82bb353bdc4afb7d53
budiariyanto <fx.budi.ariyanto@gmail.com>
2016-10-02 05:36:27 +0700

    fix sync.sh

--- sync.sh
+++ sync.sh
@@ -1,3 +1,8 @@
+hugo
 rm -rf ../budiariyanto.github.io/*
 cp -R public/* ../budiariyanto.github.io/
+cd ../budiariyanto.github.io/
+git add .
+git commit -m "update site"
+git push origin master
 echo "Sync complete"
