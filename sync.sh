79e0150caf54c0b586dbc404f68d758dae6019aa
Budi Ariyanto <fx.budi.ariyanto@gmail.com>
2017-03-22 12:01:06 +0700

    Modify sync.sh

--- sync.sh
+++ sync.sh
@@ -2,6 +2,9 @@ hugo
 mkdir ../budiariyanto.github.io
 rm -rf ../budiariyanto.github.io/*
 cp -R public/* ../budiariyanto.github.io/
+git add content/post/*.md
+git commit -m "Add articles"
+git push origin master
 cd ../budiariyanto.github.io/
 git add .
 git commit -m "update site"
